ae6a44ea9e193c8a54aa33e0be880f6b003da5a9
nektarios <nektarios.arakas@gmail.com>
2024-09-23 16:25:08 +0300

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epp_gr',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     url='https://github.com/martinikola/epp_gr.git',
     license='GPL',
