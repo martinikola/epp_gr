85012f3cd093b81bd14152e9aecb482185b2ac44
nektarios <nektarios.arakas@gmail.com>
2024-10-28 14:23:24 +0200

    * reading all ips

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epp_gr',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/martinikola/epp_gr.git',
     license='GPL',
