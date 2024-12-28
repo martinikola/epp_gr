7dbd34f1152bf9750522ffeb8fc2a359ea55d593
nektarios <nektarios.arakas@gmail.com>
2024-12-28 14:46:38 +0200

    refactor: improve XML formatting and clean up whitespace in domain and host classes

--- epp_gr/host.py
+++ epp_gr/host.py
@@ -1,16 +1,17 @@
 from ipaddress import ip_address
 from epp_gr import EppClient
 
+
 class Host:
 
     @staticmethod
-    def check(epp: EppClient, host_name:str) -> bool:
+    def check(epp: EppClient, host_name: str) -> bool:
         """ checks the availability  of a host returns True if exists"""
         xml = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
                 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
                 <command>
                     <check>
-                        <host:check xmlns:host="urn:ietf:params:xml:ns:host-1.0" 
+                        <host:check xmlns:host="urn:ietf:params:xml:ns:host-1.0"
                         xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd">
                         <host:name>{host_name}</host:name>
                         </host:check>
@@ -21,20 +22,20 @@ class Host:
         response, soup = epp.send_xml(xml)
         if epp.last_result_code == '1000':
             host_name = soup.find('host:name')
-            return host_name and host_name.get('avail') == '1'  # '0' means exists, '1' means available
+            # '0' means exists, '1' means available
+            return host_name and host_name.get('avail') == '1'
         return False
 
-
     @staticmethod
-    def info(epp: EppClient, host_name:str) -> dict:
+    def info(epp: EppClient, host_name: str) -> dict:
         """ get host info or error if not exists"""
         xml = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                <epp xmlns="urn:ietf:params:xml:ns:epp-1.0" 
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+                <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                     xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
                     <command>
                         <info>
-                            <host:info xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd" 
+                            <host:info xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd"
                                 xmlns:host="urn:ietf:params:xml:ns:host-1.0">
                         <host:name>{host_name}</host:name>
                         </host:info>
@@ -52,7 +53,8 @@ class Host:
             # ip_v6 = soup.find('host:addr', {'ip': 'v6'})
             # if ip_v6:
             #     host_info['ip_v6'] = ip_v6.text
-            host_info['ip_addresses'] = [addr.text.strip() for addr in soup.find_all('host:addr')]
+            host_info['ip_addresses'] = [addr.text.strip()
+                                                         for addr in soup.find_all('host:addr')]
             # host_info['ip_address'] = host_info['ip_addresses'][0] if host_info['ip_addresses'] else None
             return host_info
 
@@ -77,7 +79,7 @@ class Host:
         # </epp>"""
         xml = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
             <epp xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd"
-                xmlns="urn:ietf:params:xml:ns:epp-1.0"    
+                xmlns="urn:ietf:params:xml:ns:epp-1.0"
                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <command>
                 <create>
@@ -98,43 +100,51 @@ class Host:
         return epp.last_result_code == '1000'
 
     @staticmethod
-    def update(epp: EppClient, host:dict) -> bool:
-        raise NotImplementedError
-        # xml = "bla bla bla"
-        # response, soup = epp.send_xml(xml)
-        # return epp.last_result_code == '1000'
+    def update(epp: EppClient, host: dict) -> bool:
+        xml = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+            <command>
+                <update>
+                    <host:update xmlns:host="urn:ietf:params:xml:ns:host-1.0"
+                        xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd">
+                        <host:name>ns1.forth.gr</host:name>
+                        <host:add>
+                            <host:addr ip="v4">11.11.11.11</host:addr>
+                        </host:add>
+                        <host:rem>
+                        <!-- default ip type is v4 -->
+                            <host:addr>200.200.200.211</host:addr>
+                        </host:rem>
+                        <host:chg>
+                            <host:name>new.forth.gr</host:name>
+                        </host:chg>
+                    </host:update>
+                </update>
+                <clTRID>ABC:ics-forth:1079699022918</clTRID>
+            </command>
+        </epp>"""
+        response, soup = epp.send_xml(xml)
+        if epp.last_result_code == '1000':
+            return True
 
     @staticmethod
     def delete(epp: EppClient, host_name:str) -> bool:
-        xml = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-            <epp xmlns="urn:ietf:params:xml:ns:epp-1.0" 
-                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-                xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
-                <command>
-                    <delete>
-                        <host:delete xmlns:host="urn:ietf:params:xml:ns:host-1.0" 
-                            xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd">
-                        <host:name>{host_name}</host:name>
-                        </host:delete>
-                    </delete>
-                    <clTRID>{epp.clTRID}</clTRID>
-                </command>
-            </epp>"""
+        xml = f""" < ?xml version = "1.0" encoding = "UTF-8" standalone = "no"? >
+            <epp xmlns = "urn:ietf:params:xml:ns:epp-1.0"
+                xmlns: xsi = "http://www.w3.org/2001/XMLSchema-instance"
+                xsi: schemaLocation = "urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd" >
+                <command >
+                    <delete >
+                        <host: delete xmlns: host = "urn:ietf:params:xml:ns:host-1.0"
+                            xsi: schemaLocation = "urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd" >
+                        <host: name > {host_name} < /host: name >
+                        </host: delete >
+                    </delete >
+                    <clTRID > {epp.clTRID} < /clTRID >
+                </command >
+            </epp >"""
         response, soup = epp.send_xml(xml)
         if epp.last_result_code == '1000':
                 return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
